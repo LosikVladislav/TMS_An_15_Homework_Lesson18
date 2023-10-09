b4f1e4020493342aef31f7513195f21188eb17ca
LosikVladislav <vlad.losik@yandex.ru>
2023-10-10 01:07:51 +0300

    hw 18

--- app/src/main/java/com/example/tms_an_15_homework_lesson18/MainActivity.kt
+++ app/src/main/java/com/example/tms_an_15_homework_lesson18/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.tms_an_15_homework_lesson18
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 
 
+
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +22,48 @@ class MainActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.password)
         val signUp = findViewById<Button>(R.id.btn_sign_up)
 
+        val intent = Intent(this@MainActivity, ResultOfRegistration::class.java)
+
+        userName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(inputText: Editable?) {
+                val resultUserName = inputText
+                intent.putExtra("username", resultUserName.toString())
+            }
+
+        })
+
+         password.addTextChangedListener(object : TextWatcher{
+             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+             }
+
+             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+             }
+
+             override fun afterTextChanged(inputText: Editable?) {
+                 val resultUserPassword = inputText
+                 intent.putExtra("password", resultUserPassword.toString())
+             }
+         })
+
+
+
+
+
+
         signUp.setOnClickListener(){
-            if(userName.length() > 6 && password.length() > 6){
-                startActivity(Intent(this, ResultOfRegistration::class.java))
+            if(userName.length()>6 && password.length()>6){
+                startActivity(intent)
             }
         }
+
     }
 }
--- app/src/main/java/com/example/tms_an_15_homework_lesson18/ResultOfRegistration.kt
+++ app/src/main/java/com/example/tms_an_15_homework_lesson18/ResultOfRegistration.kt
@@ -2,12 +2,28 @@ package com.example.tms_an_15_homework_lesson18
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class ResultOfRegistration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_of_registration)
+
+        val resultUserName = findViewById<TextView>(R.id.resultUserName)
+        val resultUserData = intent.getStringExtra("username")
+        if(intent != null){
+            resultUserName.text ="login: " + resultUserData
+        }
+
+
+
+        val resultPassword = findViewById<TextView>(R.id.resultPassword)
+        val resultUserPassword = intent.getStringExtra("password")
+        if(intent != null){
+            resultPassword.text ="password: " + resultUserPassword
+        }
+
 
     }
 }
